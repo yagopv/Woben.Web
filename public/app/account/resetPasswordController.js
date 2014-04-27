a4b36fdfc4f5dc51492da297c41c590b08524b65
Yago Pérez Vázquez <yagoperez@outlook.com>
2014-04-27 21:52:36 +0200

    Some changes in the reset  password circuit

--- public/app/account/resetPasswordController.js
+++ public/app/account/resetPasswordController.js
@@ -2,17 +2,24 @@ WobenAccount.controller('ResetPasswordController', function($scope, $http, accou
     $scope.disabled = true;
     $scope.email = $stateParams.email;
     $scope.code = $stateParams.code;
-    
+    $scope.resetPasswordSuccess = false;
+
     $scope.resetPassword = function() {
         $scope.disabled = true;
         accountService.resetPassword($scope.email, $scope.password, $scope.confirmPassword, $scope.code).then(
             function(data) {
                 $scope.disabled = false;
+                $scope.resetPasswordSuccess = true;
+                $scope.resetPasswordForm.$setPristine();
             },function(error) {
                 $scope.modelErrors = errorService.handleAuthenticationErrors(error);
                 $scope.disabled = false;
             }
         );
+    }
+
+    $scope.closeInfo = function() {
+        $scope.mailSentSuccess = false
     }
 
     $scope.modelErrors = null;
